e56f5c2530066e3e8b212c28746ddda970de1ef4
sharnari <armgubin.sergey@gmail.com>
2024-06-25 16:31:57 +0500

    rewrite to class

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,178 +1,150 @@
 import PropTypes from 'prop-types'
-import { React, useState, useRef, useEffect } from 'react'
+import { React, Component, createRef } from 'react'
 import { formatDistanceToNow } from 'date-fns'
 import './todo-list-item.css'
 
-export default function TodoListItem(props) {
-  const {
-    id,
-    label,
-    onDeleted,
-    onToggleDone,
-    completed,
-    timer,
-    onEdit,
-    editing,
-    updateLabel,
-    timerStarted,
-    setTimerStarted,
-    accumulatedTime,
-    setAccumulatedTime,
-  } = props
-  const defaultTimer = 600 - accumulatedTime
-  const [inputValue, setInputValue] = useState(label)
-  const [timerTask, setTimerTask] = useState(defaultTimer)
-  const [timerTaskActive, setTimerTaskActive] = useState(false)
-  const inputRef = useRef(null)
-  const timerId = useRef(null)
-
-  // componentWillUnmount analog
-  useEffect(() => {
-    if (timerStarted) {
-      console.log('помнит, чтобы был нажат start', timerStarted)
-      const elapsedTime = Math.floor((new Date() - new Date(timerStarted)) / 1000)
-      setTimerTask(defaultTimer - elapsedTime)
-      setTimerTaskActive(true)
-    } else {
-      console.log('не помнит, чтобы был нажат start', timerStarted)
+export default class TodoListItem extends Component {
+  constructor(props) {
+    super(props)
+    const defaultTimer = 600 /* - this.props.accumulatedTime*/
+    this.state = {
+      inputValue: this.props.label,
+      timerTask: defaultTimer,
+      timerTaskActive: false,
     }
-    return () => {
-      if (timerTaskActive) {
-        setTimerStarted(id, new Date())
-        setTimerTaskActive(false)
-        console.log('timer deleted')
-      }
-    }
-  }, [])
+    this.inputRef = createRef()
+    this.timerId = null
+  }
 
-  useEffect(() => {
-    if (timerTaskActive) {
-      setAccumulatedTime(id, 1)
-      timerId.current = setTimeout(() => {
-        setTimerTask((prev) => {
-          if (prev > 0) {
-            return prev - 1
+  componentDidMount() {
+    this.setState({ timerTask: this.state.timerTask })
+    // if (this.props.setTimerStarted) {
+    //   const elapledTime = this.defaultTimer
+    //   this.setState({ timerTask: this.state.timerTask - elapledTime, timerTaskActive: true })
+    // }
+    // if (this.props.editing && this.inputRef.current) {
+    //   this.inputRef.current.focus()
+    // }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.timerTaskActive && !prevState.timerTaskActive) {
+      this.props.setAccumulatedTime(this.props.id, 1)
+      this.timerId = setInterval(() => {
+        this.setState((prevState) => {
+          if (prevState.timerTask > 0) {
+            return { timerTask: prevState.timerTask - 1 }
           }
-          return prev
+          return prevState
         })
       }, 1000)
-    } else {
-      clearTimeout(timerId.current)
+    } else if (!this.state.timerTaskActive && prevState.timerTaskActive) {
+      clearInterval(this.timerId)
     }
-    return () => clearTimeout(timerId.current)
-  }, [timerTask, timerTaskActive])
-
-  // focus at input place
-  useEffect(() => {
-    if (editing && inputRef.current) {
-      inputRef.current.focus()
+    if (this.props.editing && !prevProps.editing && this.inputRef.current) {
+      this.inputRef.current.focus()
     }
-  }, [editing])
-
-  useEffect(() => {
-    return () => {
-      clearTimeout(timerId.current)
-    }
-  }, [])
-
-  let classNames = ''
-  let checkedFlag = ''
-  if (completed) {
-    classNames += ' completed'
-    checkedFlag = true
   }
 
-  const handleChange = (e) => {
-    setInputValue(e.target.value)
+  componentWillUnmount() {
+    if (this.state.timerTaskActive) {
+      this.props.setTimerStarted(this.props.id, new Date())
+      this.setState({ timerTaskActive: false })
+    }
+    clearInterval(this.timerId)
   }
 
-  const handleKeyDown = (e) => {
+  handleChange = (e) => {
+    this.setState({ inputValue: e.target.value })
+  }
+
+  handleKeyDown = (e) => {
     if (e.key === 'Enter') {
-      const newLabel = inputValue
-      updateLabel(id, newLabel)
+      const newLabel = this.state.inputValue
+      this.props.updateLabel(this.props.id, newLabel)
     } else if (e.key === 'Escape') {
-      onEdit(id)
-      setInputValue(label)
+      this.props.onEdit(this.props.id)
+      this.setState({ inputValue: this.props.label })
     }
   }
 
-  const handleBlur = () => {
-    onEdit(id)
-    if (inputValue !== label) {
-      setInputValue(label)
+  handleBlur = () => {
+    this.props.onEdit(this.props.id)
+    if (this.state.inputValue !== this.props.label) {
+      this.setState({ inputValue: this.props.label })
     }
   }
 
-  // const diffSecond = (timeMore, timeLess) => {
-  //   const timeStampMore = timeMore.getTime()
-  //   const timeStampLess = timeLess.getTime()
-  //   const differentSeconds = Math.floor((timeStampMore - timeStampLess) / 1000)
-  //   return differentSeconds
-  // }
-
-  //-------------------------------------------------------------
-  const startTimer = () => {
-    if (!timerTaskActive) {
-      if (!timerStarted) {
-        setTimerStarted(id, new Date())
-        console.log('timer was start')
+  startTimer = () => {
+    if (!this.state.timerTaskActive) {
+      if (!this.props.timerStarted) {
+        this.props.setTimerStarted(this.props.id, new Date())
       }
-      setTimerTaskActive(true)
+      this.setState({ timerTaskActive: true })
     }
   }
 
-  const stopTimer = () => {
-    if (timerTaskActive) {
-      setTimerStarted(id, null)
-      setTimerTaskActive(false)
-      console.log('timer was stoped')
+  stopTimer = () => {
+    if (this.state.timerTaskActive) {
+      this.props.setTimerStarted(this.props.id, null)
+      this.setState({ timerTaskActive: false })
     }
   }
 
-  const formatTime = (seconds) => {
+  formatTime = (seconds) => {
     const mins = Math.floor(seconds / 60)
     const secs = seconds % 60
     return `${String(mins).padStart(2, '0')}:${String(secs).padStart(2, '0')}`
   }
 
-  const controlView = (editing) => {
-    if (!editing) {
-      return (
-        <>
-          <input className="toggle" type="checkbox" onChange={onToggleDone} checked={checkedFlag} />
-          <label className="list-item__content">
-            <span className="description" onClick={onToggleDone}>
-              {label}
-            </span>
-            <span className="timer">
-              <button className="icon icon-play" onClick={startTimer}></button>
-              <button className="icon icon-pause" onClick={stopTimer}></button>
-              <span className="time">{formatTime(timerTask)}</span>
-            </span>
-            <span className="created">{formatDistanceToNow(timer, { addSuffix: true, includeSeconds: true })}</span>
-          </label>
-          <button type="button" className="icon icon-edit" onClick={onEdit} />
-          <button type="button" className="icon icon-destroy" onClick={onDeleted} />
-        </>
-      )
-    } else {
-      return (
-        <div className="flexing-right">
-          <input
-            type="text"
-            className="editing"
-            value={inputValue}
-            onBlur={handleBlur}
-            ref={inputRef}
-            onChange={handleChange}
-            onKeyDown={handleKeyDown}
-          />
-        </div>
-      )
+  render() {
+    const { label, onDeleted, onToggleDone, completed, timer, onEdit, editing } = this.props
+    const { inputValue, timerTask } = this.state
+    let classNames = ''
+    let checkedFlag = ''
+    if (completed) {
+      classNames += ' completed'
+      checkedFlag = true
     }
+    const controlView = () => {
+      if (!editing) {
+        return (
+          <>
+            <input className="toggle" type="checkbox" onChange={onToggleDone} checked={checkedFlag} />
+            <label className="list-item__content">
+              <span className="description" onClick={onToggleDone}>
+                {label}
+              </span>
+              <span className="timer">
+                <button className="icon icon-play" onClick={this.startTimer}></button>
+                <button className="icon icon-pause" onClick={this.stopTimer}></button>
+                <span className="time">{this.formatTime(timerTask)}</span>
+              </span>
+              <span className="created">{formatDistanceToNow(timer, { addSuffix: true, includeSeconds: true })}</span>
+            </label>
+            {/*---------------------------warning----------------------------------*/}
+            <button type="button" className="icon icon-edit" onClick={onEdit} />
+            <button type="button" className="icon icon-destroy" onClick={onDeleted} />
+          </>
+        )
+      } else {
+        return (
+          <div className="flexing-right">
+            <input
+              type="text"
+              className="editing"
+              value={inputValue}
+              onBlur={this.handleBlur}
+              ref={this.inputRef}
+              onChange={this.handleChange}
+              onKeyDown={this.handleKeyDown}
+            />
+          </div>
+        )
+      }
+    }
+    return <div className={`view${classNames}`}>{controlView(editing)}</div>
   }
-
-  return <div className={`view${classNames}`}>{controlView(editing)}</div>
 }
 
 TodoListItem.propTypes = {
@@ -190,3 +162,10 @@ TodoListItem.propTypes = {
   setAccumulatedTime: PropTypes.func,
   accumulatedTime: PropTypes.number,
 }
+
+// const diffSecond = (timeMore, timeLess) => {
+//   const timeStampMore = timeMore.getTime()
+//   const timeStampLess = timeLess.getTime()
+//   const differentSeconds = Math.floor((timeStampMore - timeStampLess) / 1000)
+//   return differentSeconds
+// }
